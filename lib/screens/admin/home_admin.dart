2be545ca21121cf0af8bfbba2baee9bb9a98f22b
Bawantha Rajapaksha <bawwa.16@uomcse.lk>
2020-02-23 04:29:47 +0530

    ad basic views

---
+++ lib/screens/admin/home_admin.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+import 'package:flight_training/models/user_location.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
+
+class HomeAdmin extends StatefulWidget {
+  @override
+  _HomeAdminState createState() => _HomeAdminState();
+}
+
+class _HomeAdminState extends State<HomeAdmin> {
+  @override
+  Widget build(BuildContext context) {
+    var userLocation = Provider.of<UserLocation>(context);
+    return Placeholder();
+  }
+}
+
+

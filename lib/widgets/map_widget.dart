38e99d307b0b31925eef693bd1de0f5f22e878f0
Bawantha Rajapaksha <bawwa.16@uomcse.lk>
2020-02-24 23:11:21 +0530

    some changes in map

--- lib/widgets/map_widget.dart
+++ lib/widgets/map_widget.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 
+import 'package:flight_training/models/user_location.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
 
 class Map extends StatefulWidget {
   @override
@@ -24,14 +26,28 @@ class MapState extends State<Map> {
 
   @override
   Widget build(BuildContext context) {
+    var userLocation = Provider.of<UserLocation>(context);
     return new Scaffold(
       body: GoogleMap(
         mapType: MapType.normal,
-        initialCameraPosition: _initialPosition,
+        circles: Set.from(
+          [
+            Circle(
+              strokeWidth: 5,
+              strokeColor: Colors.blueAccent,
+              center: LatLng(userLocation.latitude, userLocation.longitude),
+              radius: 10,
+              circleId: CircleId("1"),
+            )
+          ],
+        ),
+        initialCameraPosition: CameraPosition(
+          target: LatLng(userLocation.latitude, userLocation.longitude),
+          zoom: 14.4746,
+        ),
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
-        
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: _goToTheLake,

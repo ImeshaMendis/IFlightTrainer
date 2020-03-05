083c3c8d10b156583e230a76deac58f6e628c430
ImeshaMendis <bawwa.16@uomcse.lk>
2020-03-05 22:46:14 +0530

    [factory added]Q

--- lib/models/q.dart
+++ lib/models/q.dart
@@ -5,5 +5,17 @@ class Q{
   final String c;
   final String correct;
 
-  Q(this.q, this.a, this.b, this.c, this.correct);
+  Q({this.q, this.a, this.b, this.c, this.correct});
+
+
+  factory Q.fromJson(Map<String,dynamic> json){
+      return Q(
+        q:json['question'] as String,
+        a:json['a'] as String,
+        b:json['b'] as String,
+        c:json['c'] as String,
+        correct:json['correct'] as String,
+
+      );
+  }
 }

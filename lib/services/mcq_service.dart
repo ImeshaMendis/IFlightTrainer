e925ae31f85bbf766bbac9e1907d94d0c9430789
ImeshaMendis <bawwa.16@uomcse.lk>
2020-03-09 19:13:42 +0530

    [mcq]CreateStream

--- lib/services/mcq_service.dart
+++ lib/services/mcq_service.dart
@@ -1,11 +1,33 @@
+import 'dart:async';
+import 'package:flight_training/models/q.dart';
+import 'package:flight_training/utils/mcq_generator.dart';
 import 'package:flutter/services.dart';
 
 class McqService {
-  static Future<String> loadAsset() async {
+
+
+
+  
+  static StreamController<List<Q>> _mcqController =
+      StreamController<List<Q>>.broadcast();
+
+  Stream<List<Q>> get mcqStream => _mcqController.stream;
+
+
+   Future<String> loadAsset() async {
     try {
       return await rootBundle.loadString('lib/utils/mcq.json');
     } catch (e) {
-      print("error read dara");
+      print("error read data");
     }
   }
+
+    static void getRandomMcqs(){
+     McqGenerator.genRandomMcqs().then((mcqs){
+        if(mcqs!=null){
+            _mcqController.add(mcqs);
+        }
+     });
+   }
+  
 }

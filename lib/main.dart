ac10ca2d77add19367b614ffa6246b5306f8ad2b
ImeshaMendis <bawwa.16@cse.mrt.ac.lk>
2020-03-17 23:36:22 +0530

    [routes]addedNewRoutes

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flight_training/screens/admin/home_admin.dart';
 import 'package:flight_training/screens/exam.dart';
 import 'package:flight_training/screens/login.dart';
 import 'package:flight_training/screens/student/home_student.dart';
+import 'package:flight_training/services/date_pass_service.dart';
 import 'package:flight_training/services/location_service.dart';
 import 'package:flight_training/widgets/map_widget.dart';
 import 'package:flutter/material.dart';
@@ -15,18 +16,17 @@ void main() => runApp(FlightApp());
 class FlightApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Login(),
-        initialRoute: "/",
-        routes: {
-          '/admin': (context) => HomeAdmin(),
-          // When navigating to the "/second" route, build the SecondScreen widget.
-          '/student': (context) => HomeStudent(),
-          '/admin_lesson_view': (context) => AdminLessonView(),
-          '/start_exam':(context)=>Exam()
-        },
-      
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Login(),
+      initialRoute: "/",
+      routes: {
+        '/admin': (context) => HomeAdmin(),
+        // When navigating to the "/second" route, build the SecondScreen widget.
+        '/student': (context) => HomeStudent(),
+        '/admin_lesson_view': (context) => AdminLessonView(),
+        '/start_exam': (context) => Exam()
+      },
     );
   }
 }

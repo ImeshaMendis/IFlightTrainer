e00d05df9588705c2adcdb8c79e5c34309eb9b40
ImeshaMendis <bawwa.16@cse.mrt.ac.lk>
2020-04-01 01:43:04 +0530

    [modal]update

--- lib/models/lesson.dart
+++ lib/models/lesson.dart
@@ -1,11 +1,16 @@
+
+
+import 'package:flight_training/utils/states.dart';
+
 class Lesson {
   final String date;
   final String lessonId;
   String time = "Set Time";
-  Lesson({this.date, this.lessonId});
+  LessonState state; 
+  Lesson({this.date, this.lessonId, this.state});
 
   factory Lesson.fromJson(Map<String, dynamic> json) {
     return Lesson(
-        date: json['date'] as String, lessonId: json['lessonId'] as String);
+        date: json['date'] as String, lessonId: json['lessonId'] as String, state: json['lessonState']);
   }
 }

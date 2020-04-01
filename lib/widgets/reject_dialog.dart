dec65b84d73a1ecb2388fced313a99fca6ebb760
ImeshaMendis <bawwa.16@cse.mrt.ac.lk>
2020-04-01 14:09:39 +0530

    new dialog to reject booking

---
+++ lib/widgets/reject_dialog.dart
@@ -0,0 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+Future<void> rejectnotice(
+    BuildContext context, DocumentSnapshot snapshot) async {
+  final controller = TextEditingController();
+
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('PLEASE ADD A REJCT NOTE'),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              TextField(
+                controller: controller,
+              ),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('OKAY'),
+            onPressed: () async {
+              Navigator.of(context).pop();
+              Firestore.instance
+                  .collection('lesson')
+                  .document(snapshot.documentID)
+                  .updateData({
+                "notice": controller.text.toString(),
+                "state": "DECLINED"
+              });
+            },
+          ),
+        ],
+      );
+    },
+  );
+}

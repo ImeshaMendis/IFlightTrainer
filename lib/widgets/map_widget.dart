906649aee70cf9a1b93bc75bc641f6e347df0c51
ImeshaMendis <bawwa.16@cse.mrt.ac.lk>
2020-04-07 01:58:43 +0530

    result_chartAdded

--- lib/widgets/map_widget.dart
+++ lib/widgets/map_widget.dart
@@ -13,52 +13,41 @@ class Map extends StatefulWidget {
 class MapState extends State<Map> {
   Completer<GoogleMapController> _controller = Completer();
 
-  static final CameraPosition _initialPosition = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(6.820771, -79.888508),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
   @override
   Widget build(BuildContext context) {
     var userLocation = Provider.of<UserLocation>(context);
+
     return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.normal,
-        circles: Set.from(
-          [
-            Circle(
-              strokeWidth: 5,
-              strokeColor: Colors.blueAccent,
-              center: LatLng(userLocation.latitude, userLocation.longitude),
-              radius: 10,
-              circleId: CircleId("1"),
-            )
+      body: Center(
+        child: Stack(
+          children: <Widget>[
+            GoogleMap(
+              
+              mapType: MapType.normal,
+              circles: Set.from(
+                [
+                  Circle(
+                    strokeWidth: 5,
+                    strokeColor: Colors.blueAccent,
+                    center:
+                        LatLng(userLocation.latitude, userLocation.longitude),
+                    radius: 10,
+                    circleId: CircleId("1"),
+                  )
+                ],
+              ),
+              initialCameraPosition: CameraPosition(
+                target: LatLng(userLocation.latitude, userLocation.longitude),
+                zoom: 14.4746,
+              ),
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+            ),
+            Text("HEIGHT" + userLocation.head.toString()),
           ],
         ),
-        initialCameraPosition: CameraPosition(
-          target: LatLng(userLocation.latitude, userLocation.longitude),
-          zoom: 14.4746,
-        ),
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
       ),
     );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
